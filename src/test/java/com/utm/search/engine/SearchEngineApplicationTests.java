71319127d9ee1970d273a4f0e2c3242a6eeb9064
victornani <victor.nani.cs@gmail.com>
2023-03-19 18:00:28 +0200

    Created a basic Spring Boot application for the Search Engine

---
+++ src/test/java/com/utm/search/engine/SearchEngineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.utm.search.engine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SearchEngineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
